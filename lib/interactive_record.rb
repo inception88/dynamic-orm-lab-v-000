8b8b4d5f95f9c3bc64fcbf7bba90a4c1bef11a1f
Benjamin Jones <bljones36@gmail.com>
2018-11-06 00:37:38 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,8 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute)
-    attribute.each {|key, value| k = key, v = value}
+    attribute.each {|key, value| k = key
+      v = value}
     sql = "SELECT * FROM #{self.table_name} WHERE ? = '#{v}'"
     DB[:conn].execute(sql, k)
   end
